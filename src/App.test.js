5ea33a1ea1ae867994950a655b60fb78abf76fd4
Agung Adi Prasetio <prasetioxagung@gmail.com>
2020-11-27 13:48:08 +0700

    tambah testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+import {Provider} from 'react-redux'
+import store from './store' 
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('testing App component', () => {
+  test('should have Home text/link', () => {
+    const {getByText} = render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>)
+    const textAnchor = getByText('Home')
+    expect(textAnchor).toBeInTheDocument()
+  })
+  test('should have Favorite text/link', () => {
+    const {getByText} = render(<Provider store={store}><BrowserRouter><App/></BrowserRouter></Provider>)
+    const textAnchor = getByText('Favorite')
+    expect(textAnchor).toBeInTheDocument()
+  })
+})
